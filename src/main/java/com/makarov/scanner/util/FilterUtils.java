720c16371c00cf7b46daf98a5c55d2e1a188d55f
amakaroff <alekseymakarov1996@gmail.com>
2017-04-30 22:50:47 +0400

    Fix name methods

--- src/main/java/com/makarov/scanner/util/FilterUtils.java
+++ src/main/java/com/makarov/scanner/util/FilterUtils.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Annotation;
 
 public class FilterUtils {
 
-    public static boolean annotationFilter(Class<? extends Annotation>[] annotations, Class<?> clazz) {
+    public static boolean isAnnotationsPresent(Class<? extends Annotation>[] annotations, Class<?> clazz) {
         for (Class<? extends Annotation> annotation : annotations) {
             if (clazz.isAnnotationPresent(annotation)) {
                 return true;
@@ -15,11 +15,11 @@ public class FilterUtils {
         return false;
     }
 
-    public static boolean classNameFilter(String className, Class<?> clazz) {
+    public static boolean isClassNameContains(String className, Class<?> clazz) {
         return clazz.getName().contains(className);
     }
 
-    public static boolean classFilter(Class<?> superClass, Class<?> clazz) {
-        return (clazz.isAssignableFrom(superClass));
+    public static boolean isAssignableFrom(Class<?> superClass, Class<?> clazz) {
+        return clazz.isAssignableFrom(superClass);
     }
 }
